90943d45462fa3c31fe81b410d2d4fadfae9ab07
Guilherme Simoes <guilhermejunios@gmail.com>
2022-05-25 13:39:58 -0300

    componente: Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-           <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />          
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />    
+        <link rel="stylesheet" href="/favicon.png" type="image/png"/>      
       </Head>      
       <body>
         <Main />
