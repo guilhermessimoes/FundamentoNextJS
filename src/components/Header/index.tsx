be45c7886ea8f09767c769cd0183cdee44f0c4d8
Guilherme Simoes <guilhermejunios@gmail.com>
2022-05-25 21:23:45 -0300

    Página home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss'
 
 export function Header(){
   return (
-    <header className="styles.headerContainer">
-      <div className="styles.headerContent">
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
+      
+        <nav>
+          <a href="" className={styles.active}>Home</a>
+          <a href="">Posts</a>
+        </nav>
+        <SignInButton />
       </div>
-      <nav>
-        <a href="" className={styles.active}>Home</a>
-        <a href="">Posts</a>
-      </nav>
     </header>
   );
 }
